c92ba00b564678f0905a00fb90430e32c479ded8
dtarjeft <dtarjeft@gmail.com>
2015-08-21 10:35:14 -0400

    It's going well!

---
+++ NugetDickery/ProjectOne/ProjectFour/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFour
+{
+    public class Class1
+    {
+    }
+}
